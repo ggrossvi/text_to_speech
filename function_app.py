7524f000d9a417a0c59030301447a3774e2a1c31
Gloria Villa <hello.gloria@gmail.com>
2024-06-11 15:24:12 -0700

    changed parameter

--- function_app.py
+++ function_app.py
@@ -7,17 +7,17 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def func_text_to_speech(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    name = req.params.get('text')
+    if not text:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get('text')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {text}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
